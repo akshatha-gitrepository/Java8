c95d2c94955b1c5ec5427b2d3e2d746a18e125a4
akshatha_achar <P1815AS@msc.corpdir.net>
2025-09-13 23:54:00 +0530

    learning Data structure

---
+++ DSA/src/main/java/SparkCourse/Main.java
@@ -0,0 +1,17 @@
+package SparkCourse;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        LinkedList ls = new LinkedList(10);
+        ls.printList();
+        ls.getHead();
+        ls.getLength();
+        ls.getTail();
+    }
+
+
+
+
+}
